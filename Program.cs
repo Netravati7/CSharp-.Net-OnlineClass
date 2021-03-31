b33c7ca08701ac6c135ab221cf0431b0b5082280
Netravati7 <67043742+Netravati7@users.noreply.github.com>
2021-03-31 18:01:44 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace StudentApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Details of Student..");
+            //create an array of student object   
+            Student[] details = new Student[3];
+            //create actual student objects using constructor  
+            details[0] = new Student();
+            details[1] = new Student();
+            details[2] = new Student();
+            //display the student object data  
+            details[0].displayInfo();
+            details[1].displayInfo();
+            details[2].displayInfo();
+            details[0].displayCGPA();
+            details[1].displayCGPA();
+            details[2].displayCGPA();
+            Console.ReadLine();
+        }
+        
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace StudentApp
+{
+    class Student
+    {
+        /*2.Student Details - Netra
+        Name
+        class
+        M1
+        M2
+        M3
+        cgpa - private
+        calcualateCGPA - calculates the CGPA (M1+M2+M2/3)
+        Display Details - should display all the details*/
+
+        public string name;
+        public string classOfStudent;
+        public int marks1;
+        public int marks2;
+        public int marks3;
+        private int cgpa;
+
+       public void studentInfo()
+        {
+            Console.WriteLine("Enter the student Name:");
+            name = Console.ReadLine();
+            Console.WriteLine("Enter the student class:");
+            classOfStudent = Console.ReadLine();
+            Console.WriteLine("Enter the marks1:");
+            marks1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the marks2:");
+            marks2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the marks3:");
+            marks3 = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public void calcualateCGPA()
+        {
+            cgpa = (marks1 + marks2 + marks3) / 3;
+            Console.WriteLine("CGPA of "+name+":"+cgpa);
+        }
+        public int CGPA
+        {
+            get { return cgpa; }
+            set { cgpa = value; }
+        }
+
+        public void displayInfo()
+        {
+            studentInfo();
+        }
+
+        public void displayCGPA()
+        {
+            calcualateCGPA();
+        }
+    }
+}
